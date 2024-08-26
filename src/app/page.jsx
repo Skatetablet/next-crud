177f0bc8b60da269ade0f2fbec2ed67486c3a86a
Skatetablet <skatewer@gmail.com>
2024-08-26 14:16:18 -0700

    Fixed fetch for when server not running

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -18,11 +18,13 @@ const HomePage = () => {
 
   return (
     <section className='container mx-auto'>
-      <div className='grid grid-cols-3 gap-3 mt-10'>
-        {tasks.map((task) => (
-          <TaskCard task={task} key={task.id} />
-        ))}
-      </div>
+      {tasks && (
+        <div className='grid grid-cols-3 gap-3 mt-10'>
+          {tasks.map((task) => (
+            <TaskCard task={task} key={task.id} />
+          ))}
+        </div>
+      )}
     </section>
   );
 };
